231aca9e7206889a2479871519cc7ba7c1a8b8b6
Zain Umerani <zumerani@gmail.com>
2016-01-29 15:07:15 -0800

    transformed from static --> angular (views, controllers, routing, etc.)

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name siggwebsiteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the siggwebsiteApp
+ */
+angular.module('siggwebsiteApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
