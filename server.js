c7874152dc3c199de2842684ecf171e55193c3c2
bryhlee <bhlee@scu.edu>
2016-03-15 20:02:54 -0700

    Loading time improvements + server.js fix

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
-//Dependencies
-
+/* -------- LOAD EXPRESS-------- */
 var express = require('express');
 var server = express();
 
-var bodyParser = require('body-parser');
-var path = require('path');
-
-
-server.use(bodyParser.urlencoded( {extended: true }));
-
-server.use(bodyParser.json() );
-
-/* -------- ROUTES -------- */
-
+/* -------- SERVE FILES -------- */
 server.use(express.static(__dirname + '/'));
-server.use(function(req, res) {
+server.use(function(sreq, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
+/* -------- DEPENDENCIES -------- */
+var bodyParser = require('body-parser');
+var path = require('path');
 
+server.use(bodyParser.urlencoded({extended: true }));
+server.use(bodyParser.json() );
+
+
+/* -------- RUN LOCAL SERVER -------- */
 server.listen(process.env.PORT || 8080, function () {
     console.log("Magic happens at ", process.env.PORT || 8080);
 });
